15c8fb7125de31fef68e26961eebc6bdfe8ae242
James Lemieux <jimmygtr11@gmail.com>
2022-07-17 20:32:05 -0400

    removes existing addons first

--- scripts/copy_mitm.sh
+++ scripts/copy_mitm.sh
@@ -6,6 +6,7 @@ ADDONS_TARGET=/home/pi/.mitmproxy/custom-addons
 CONFIG_SOURCE=$APP_DIR/mitm/config.yml
 CONFIG_TARGET=/home/pi/.mitmproxy/config.yml
 
+rm -rf $ADDONS_TARGET/*
 cp $ADDONS_SOURCE/*.py $ADDONS_TARGET/
 
 cp $CONFIG_SOURCE $CONFIG_TARGET
